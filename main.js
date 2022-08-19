4265ab7f32877e03c27a832437a8c3534432387e
ignacio.madruga <ignaciomadruga1989@gmail.com>
2022-08-19 03:48:22 -0300

    segundo commit js

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("hola mundo")
+/*  let numero1 = 20;      
+let numero2 = 10.5;
+let nombre = "jorge";
+let apellido = "perez";
+
+let resultado = numero1 + numero2;
+
+let saludo = "Hola"
+let saludo2 = "a"
+let saludo3 = " todo el"
+let blanco = " "
+
+let msg = saludo+ blanco + saludo2 + blanco + saludo3; */ 
