0dbbb2b628523c8c8a7ddecbbd4d1a211b30a521
ignacio.madruga <ignaciomadruga1989@gmail.com>
2022-08-23 12:16:26 -0300

    cambiando comillas

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let msge1="hola";
-let msge2="el resultado";
-let msge3= "de la multiplicación de";
-let msge4= "es";
-let Blanco= " ";
-let ingresarNumero = parseInt(prompt("Ingresar Numero"));
+let msge1=`hola`;
+let msge2=`el resultado`;
+let msge3= `de la multiplicación de`;
+let msge4= `es`;
+let Blanco= ` `;
+let ingresarNumero = parseInt(prompt(`Ingresar Numero`));
 
  for (let i = 1; i <= 10; i++) {
     let resultado = ingresarNumero * i ;
-    console.log( msge1 + Blanco+ msge2 + Blanco+msge3 + Blanco+ ingresarNumero +" * "+ i + Blanco+ msge4  + " = " +  resultado);
+    console.log( msge1 + Blanco+ msge2 + Blanco+msge3 + Blanco+ ingresarNumero +` * `+ i + Blanco+ msge4  + ` = ` +  resultado);
 }
