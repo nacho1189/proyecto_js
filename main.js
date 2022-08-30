f5bbe19569e60bb8297995ac2c3afcd13e7912b9
ignacio.madruga <ignaciomadruga1989@gmail.com>
2022-08-30 18:59:05 -0300

    segundo entregable js 3 cambio

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 function solicitarNombre(){
-    let nombreIngresado   = prompt(`Ingresar nombre`)
+    var nombreIngresado = prompt(`Ingresar nombre`)
     alert(`Hola estas en el cotizador ` + nombreIngresado)
 } 
 solicitarNombre();
@@ -21,4 +21,28 @@ switch (seleccion) {
     default:
         break;
         
+}
+let otraCotizacion = prompt(`Desea otra cotización?`);
+
+if((otraCotizacion == "SI") || (otraCotizacion =="si")){
+
+solicitarNombre();   
+const cotizacionDolar = 40;
+const cotizarDolar = (pesos) => pesos / cotizacionDolar;
+const cotizarPesos = (dolar) => dolar * cotizacionDolar;
+let seleccion = prompt(`Selecciona cotizacion \n 1 - Dolar a peso \n 2 -  Peso a dolar `);
+let valor = prompt("VALOR");
+switch (seleccion) {
+    case "1":
+        alert(cotizarPesos(valor));
+        break;
+    case "2":
+        alert(cotizarDolar(valor));
+        break;
+    default:
+        break;
+        
+} 
+}else{
+    alert("Adios"); 
 }
