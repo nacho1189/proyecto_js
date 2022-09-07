492865dbd9a36dcd7ceec72a5f4ff54b2ff0372c
ignacio.madruga <ignaciomadruga1989@gmail.com>
2022-09-07 16:01:40 -0300

    primera entrega proyecto js.4

--- js/main.js
+++ js/main.js
@@ -13,18 +13,15 @@ const productos =[
         marca: 'Samsung',
         color: 'Gris',
     },
+    {
+
+        id: 3,
+        producto: 'heladera',
+        marca: 'LG',
+        color: 'Blanco',
+    },
 ];
 
-const marcas = [
-    'Samsung',
-    'Philips',
-    'LG',
-];
-const colores = [
-    'Negro',
-    'Gris',
-    'Blanco',
-];
 
 console.log(productos);
 
@@ -33,18 +30,13 @@ while(option !== 0) {
     option =Number(prompt(`Selecciona una opción \n 1 - Comprar Prducto \n 2 - Ver producto \n 3 - Borrar producto \n 0 -  Salir `));
     switch (option) {
         case 1:
-            const producto = Number(prompt('Ingrese el producto que desea agregar \n 1 - Televisor \n 2 - Celular'))
-            const color = prompt('Ingrese el color del producto');
-            const marca = Number(prompt('Ingrese la marca del producto \n 1 - Philips \n 2 - Samsung'))
-            const id = getLastID()+ 1;
-            createProducto(id,producto, marca, color);
+            const productos = Number(prompt('Ingrese el producto que desea agregar \n 1 - Televisor \n 2 - Celular\n 3 - Heladera'))
             break;
         case 2:
-            
             getAllPrductos();
             break;
         case 3:
-            let deleteID = Number(prompt('Ingrese el ID de la tarea a eliminar: '));
+            let deleteID = Number(prompt('Ingrese el ID del producto a eliminar:'));
             deletePrductos(deleteID);
         case 0:
             alert('Gracias regrese pronto ');
@@ -56,26 +48,10 @@ while(option !== 0) {
     
 }
 
-function createProducto(id, pruducto, marca, color) {
-    productos.push({
-        id,
-        pruducto,
-        marca,
-        color: colores[color - 1] 
-    });
-}
-
-function getAllPrductos() {
-    console.log('\n------------------------------\n');
-    productos.forEach((producto) => console.log(producto.id + " - " + marcas[producto.marca - 1] + " - " + producto.producto + " - " + producto.color));
-}
-function getLastID(){
-    return productos.length;
-}
 function deletePrductos(id) {
     productos = productos.filter(producto => producto.id != id); 
 }
-function getLastID() {
-    const ids = productos.map(producto => producto.id);
-    return Math.max(...ids);
+function getAllPrductos() {
+    console.log();
+    productos.forEach((producto) => console.log(producto.id + " - " + producto.marca  + " - " + producto.producto + " - " + producto.color));
 }
